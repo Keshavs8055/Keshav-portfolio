485f2d942f51e42a58ad90a03184d5b61089517f
Keshav Sharma <keshavs8055@gmail.com>
2022-02-18 14:09:49 +0530

    Updated project card

--- src/components/slides/project.tsx
+++ src/components/slides/project.tsx
@@ -1,11 +1,11 @@
 import React from "react";
 import { ProjectCard } from "../Project-card/card";
 // IMGS
+import POP_IMG from "../../imgs/Project Thumb/POP-1.png";
 import GotNote from "../../imgs/Project Thumb/Got-Note.png";
 import STP from "../../imgs/Project Thumb/STP.png";
 import DICT from "../../imgs/Project Thumb/DICT.png";
 import ScrollAnimation from "react-animate-on-scroll";
-import { PopThoughtCard } from "../popCard/card";
 
 interface IProject {
   projectRef: React.RefObject<HTMLDivElement>;
@@ -24,7 +24,25 @@ export const ProjectContent: React.FC<IProject> = ({ projectRef }) => {
         <h1 className="fw-lighter text-center pb-2 text-light">Projects</h1>
       </ScrollAnimation>
       <div className="row row-cols-2 project-row py-1 justify-content-evenly">
-        <PopThoughtCard />
+        <ProjectCard
+          visitLink="https://pop-thoughts.vercel.app/"
+          content=" A social media application
+          built with
+          react and firebase.
+          You can sign up with your
+            email or google account
+          and start posting. The user can also view their posted thoughts
+          and update them from their profile menu.
+        
+          This site uses
+          React with Material-UI
+          for an enhanced user experience. The Redux library is used to
+          handle the state throughout the application. Each thought posted
+          is stored on the firestore from where it can be easily accessed.
+        "
+          img={POP_IMG}
+          title="Pop Thoughts"
+        />
         <ProjectCard
           visitLink="https://notes-app-six-psi.vercel.app/"
           content="A simple note taking application which allows users to signIn with their google account. The app offers the functionality to add, delete notes which are then stored in firebase. The web app uses React, Redux and Material-Ui"
