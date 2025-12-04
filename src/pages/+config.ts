6b2031f1882f97be04841655f2d2d075f62219b1
Keshav <keshavs.0496@gmail.com>
2025-12-04 22:45:12 +0530

    Updated title and description.

--- src/pages/+config.ts
+++ src/pages/+config.ts
@@ -4,8 +4,7 @@ import vikeReact from "vike-react/config";
 export default {
   extends: [vikeReact],
   prerender: true,
-  title:
-    "WebTuner – Web Infrastructure Specialist | SEO, AI-Readiness & Performance Audits",
+  title: "WebTuner | SEO, AI-Readiness & Performance Audits",
   description:
-    "Make your website visible to AI tools like ChatGPT, Perplexity and Google’s AI search. I fix performance, SEO, metadata, structured data and Core Web Vitals. Get a free audit and improve your rankings fast.",
+    "Make your website visible to AI tools like ChatGPT, Perplexity and Google’s AI search. I fix performance, SEO, metadata, structured data and Core Web Vitals.",
 } satisfies Config;
